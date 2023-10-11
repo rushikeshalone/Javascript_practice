370dc5602a1a8bb95b2c2cf46143fcf98f2fc8f5
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-11 14:06:24 +0000

    update

--- advance/object2.js
+++ advance/object2.js
@@ -28,7 +28,16 @@ const regularUser = {
 
 
 }
-console.log(regularUser)
-console.log(regularUser.fullname.username.firstName)
-console.log(regularUser.fullname.user.name)
+// console.log(regularUser)
+// console.log(regularUser.fullname.username.firstName)
+// console.log(regularUser.fullname.user.name)
+
+const obj1 ={1:"a", 2:"b", 3:"c"}
+const obj2 = {4:"d",5:"e"}
+
+//const obj3 = {obj1 , obj2}            // these is wrong 
+ 
+const obj3 = Object.assign(obj1,obj2)
+console.log(obj3)
+
 
