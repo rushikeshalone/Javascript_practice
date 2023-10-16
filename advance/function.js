e13648ed97be3f6ddb72ad27a09730b65f5af2d2
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-16 13:37:45 +0000

    Function video one completed

--- advance/function.js
+++ advance/function.js
@@ -8,4 +8,84 @@ function myname(){
     console.log("I")
 }
 
-myname()
+// myname()
+
+// function addtwonumber(number1,number2){
+
+//     console.log(number1 + number2)
+
+// }
+
+// addtwonumber(3,4)
+// addtwonumber(3,"4")
+// addtwonumber(3,null)
+
+function addtwonumber(number1,number2){     // number1 and number2 are called as parameter of function
+
+    // let result = number1 + number2       
+    // return result
+
+    return number1 + number2
+}
+
+const result = addtwonumber(4,6)           //        4 or 6 are called as argument of function
+
+// console.log("Result:-" ,result)
+
+
+
+//first retun function 
+
+
+function loginUserMassage(username){
+    return `${username} just Loged In`
+
+}
+
+const resultlogin = loginUserMassage("akshay") 
+const resultlogin2 = loginUserMassage() 
+// akshay name ki jagah agar  kuch pass nhi hua empty string pass hue to 
+// values undefined aayegi
+
+// console.log(resultlogin)
+// console.log(resultlogin2)
+
+// Secound   return function 
+
+function loginUserMassage2(username){
+    if (username === undefined){
+        console.log(`Please Insert Some Values`);
+        return
+    }
+    return `${username} just Loged In`
+
+}
+
+// console.log(loginUserMassage2())
+
+// third retun function
+
+function loginUserMassage3(username){
+    if (!username){
+        console.log(`Please Insert Some Values`);
+        return
+    }
+    return `${username} just Loged In`
+
+}
+
+// console.log(loginUserMassage3())
+
+// fourth function written  
+// default values pass
+
+function loginUserMassage4(username = "Sam"){
+    if (!username){
+        console.log(`Please Insert Some Values`);
+        return
+    }
+    return `${username} just Loged In`
+
+}
+console.log(loginUserMassage4())
+console.log(loginUserMassage4("Rushikesh"))
