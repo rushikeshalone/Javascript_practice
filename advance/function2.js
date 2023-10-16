a034492b55467bd3496a7f33873b07d95f3857cc
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-16 14:05:44 +0000

    function2

---
+++ advance/function2.js
@@ -0,0 +1,22 @@
+function calculatecartprice(...num1){
+    
+    return num1
+
+}
+
+// note :-  ... three dot are spread operator or rest operator its use depend 
+
+//  rest operator :- jo bhi bikhare hue hai usako sametke ek bundle karke do
+//   spread operator :- 
+
+console.log(calculatecartprice(200,400,500));
+
+
+function calculatecartprice2(val1,val2,...num1){
+    
+    return num1
+
+}
+console.log(calculatecartprice2(200,400,500,20000))
+
+//note :-  val1 and val2 are  200 or 400  values assign hue hai 
