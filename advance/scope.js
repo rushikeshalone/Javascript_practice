c2e8933bc9168cc3b80b3bd4989cb9f480a984e2
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-17 08:32:37 +0000

    sCOPE

---
+++ advance/scope.js
@@ -0,0 +1,14 @@
+
+var c = 500
+let a = 300
+
+ if (true){
+    let a = 30
+    const b = 40
+    var c = 50  
+    console.log("inner :", a) 
+ }
+ console.log(a)
+ 
+//  console.log(b)
+ console.log(c)
