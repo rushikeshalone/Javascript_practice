9f501551211e096bd055b1a02eed5aa10e555cef
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-17 06:10:21 +0000

    Function End

--- advance/function2.js
+++ advance/function2.js
@@ -9,7 +9,7 @@ function calculatecartprice(...num1){
 //  rest operator :- jo bhi bikhare hue hai usako sametke ek bundle karke do
 //   spread operator :- 
 
-console.log(calculatecartprice(200,400,500));
+//  console.log(calculatecartprice(200,400,500));
 
 
 function calculatecartprice2(val1,val2,...num1){
@@ -17,6 +17,30 @@ function calculatecartprice2(val1,val2,...num1){
     return num1
 
 }
-console.log(calculatecartprice2(200,400,500,20000))
+// console.log(calculatecartprice2(200,400,500,20000))
 
 //note :-  val1 and val2 are  200 or 400  values assign hue hai 
+
+const user = {
+    username:"Rushikesh",
+    Price: 199
+}
+
+function handleObject (anyObject){
+console.log(`username is ${anyObject.username}  and price is ${anyObject.Price} `)
+}
+
+// handleObject(user)
+handleObject({
+    username:"Tekade",
+    Price:2585
+})
+
+const mynewarray =[12,21,21,2,2654,454,656,4646]
+
+function retunnewvalues (getarray){
+    return getarray[7]
+}
+
+// console.log(retunnewvalues(mynewarray))
+console.log(retunnewvalues([12,21,21,2,2654,454,656,4646]))
