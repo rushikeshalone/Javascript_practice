132c0a024c4cfcc7c5a0f82c8eb6f35ca679957c
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-18 11:18:03 +0000

    changes

--- advance/arrow.js
+++ advance/arrow.js
@@ -4,9 +4,42 @@ const user = {
 
     welcomemessage : function(){
    console.log(`${this.username}, welcome to Website`)
+//    console.log(this)
     }
 }
 
-user.welcomemessage()
-user.username = "JAyesh"
-user.welcomemessage()
+// user.welcomemessage()
+// user.username = "JAyesh"
+// user.welcomemessage()
+// console.log(this)
+
+
+// function one(){
+//     let username = "Alone"
+//     console.log(this.username)
+// }
+
+// one()
+
+
+// const chai = function (){
+//     let username = "Alone"
+//     console.log(this.username)
+// }
+
+// chai()
+
+//  Arrow Function written as
+
+const chai = () => {
+    let username = "Alone"
+    console.log(this.username)
+}
+
+chai()
+
+const addtwo = (num1,num2) => {
+    return num1 + num2 
+
+}
+console.log(addtwo (3,6))
