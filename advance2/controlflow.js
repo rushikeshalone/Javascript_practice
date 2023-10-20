3a66e52bf8c304508a3eef9b8175327379f41f64
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-20 05:43:29 +0000

    Secount flow

--- advance2/controlflow.js
+++ advance2/controlflow.js
@@ -22,22 +22,22 @@ if (tempreture === 40){
 }else{
     //console.log("tempreture is greter than 50")
 }
-
-// const score = 500;
-// if(score > 100){
-// const power ="fly";
-// console.log(`user Power ${power}`)
-// }
-
-
-// const score = 500;
-// if(score > 100){
-// const power ="fly";
-// console.log(`user Power ${power}`)
-// }
-// console.log(`user Power ${power}`)
+/*
+const score = 500;
+if(score > 100){
+const power ="fly";
+console.log(`user Power ${power}`)
+}
 
 
+const score = 500;
+if(score > 100){
+const power ="fly";
+console.log(`user Power ${power}`)
+}
+console.log(`user Power ${power}`)
+
+*/
 
 /*  implicit scope  */
 
@@ -45,6 +45,7 @@ const balance = 1000;
 
 // if(balance > 500) console.log("test");   these is bad practice 
 
+/*
 
 if (balance < 500 ){
     console.log("less than 500")
@@ -54,4 +55,75 @@ if (balance < 500 ){
     console.log("less than 900")
 }else{
     console.log("less than 20000")
+}
+
+const userLoggedin = true;
+const debitcard = true;
+const LOgInformGoogle = false;
+const logInforemail =true;
+if (userLoggedin && debitcard){
+    console.log("Allowed to course bye");
+}
+
+if(LOgInformGoogle || logInforemail){
+    console.log("User LOgged in ");
+}
+
+ */
+
+//  Switch cases  program
+
+/*
+switch (key){
+    case value:
+
+    break;
+    default:
+
+    break;
+}
+
+*/
+/*
+const month =4
+
+switch(month){
+    case 1 :
+        console.log("January");
+        break;
+    case 2 :
+        console.log("Feb");
+        break;
+    case 3 :
+        console.log("March");
+        break;
+    case 4 :
+        console.log("April");
+        break;
+
+        default:
+            console.log("Default case")
+         break;
+}
+*/
+
+const month = "Feb"
+
+switch(month){
+    case "Jan" :
+        console.log("January");
+        break;
+    case "Feb" :
+        console.log("Feb");
+        break;
+    case "March" :
+        console.log("March");
+        break;
+    case "April" :
+        console.log("April");
+        break;
+
+        default:
+            console.log("Default case")
+         break;
 }
