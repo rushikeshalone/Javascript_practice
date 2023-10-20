af696f1d2e99e8b88a22f3c54e6fedd5e7aae53f
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-20 13:29:38 +0000

    loop

--- loop/loop1.js
+++ loop/loop1.js
@@ -1,8 +1,42 @@
  // for loop
 
+/*
  for (let i = 0; i < 10; i++) {
     const element = i;
-    console.log(element);
-    
+
+     if (element == 5){
+      console.log("5 is the best number")
+  
+     }
+     console.log(element);
  }
 
+ */
+/*
+
+ for (let i = 0; i <= 10; i++) {
+   console.log(`Outer loop ${i} `)
+   for (let j = 0; j < 10; j++) {
+      //console.log(`Inner  loop ${j} and inner loop ${i} `)
+     console.log(i + '*' + j + '=' + i* j );
+
+   }
+ } 
+ */
+
+ /*
+ for (let i = 2; i <= 10; i++ ) {
+ for (let j = 1; j <= 10; j++) {
+     console.log( i + '*' + j + '=' + i*j )
+ }
+   
+ }
+ */
+
+ for (let i = 0; i < 5; i++) {
+   for (let j = 0; j < array.length; j++) {
+      const element = array[j];
+      
+   }
+   
+ }
