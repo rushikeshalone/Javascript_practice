404e3ca973f535a96e9e9b4860b8be79a89ad54e
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-20 09:18:38 +0000

    trusty

---
+++ advance2/trusty.js
@@ -0,0 +1,38 @@
+const useremail = "arushi@gmasil.com"
+
+// if (useremail){
+//     console.log("Got User Email");
+// }else{
+//     console.log("Dont have user email");
+// }
+
+
+/*
+** falsy values **
+
+ false , 0 , -0 , BigInt 0n , "" , null , Undefined , NaN 
+
+ ** truthy values **
+
+ other values are truthy values
+
+ "0"  ===> truthy values
+ 'False' ===> truthy values
+ " " ===> truthy values
+ [] , { } , function(){}
+
+*/
+
+/*
+const email2 = []
+if(email2 === 0){
+    console.log("Array is empty")
+}
+
+*/
+
+const emptyobject = {}
+
+if (emptyobject === 0 ){
+ console.log("Object is Empty")
+}
