859b31234ed41e798769878648eb9363ab20f96e
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-21 13:20:56 +0000

    while

---
+++ loop/do_while_loop.js
@@ -0,0 +1,27 @@
+
+// while loop
+
+/*
+
+let index = 0;
+while (index <= 10) {
+    console.log(`values of log is ${index}`)
+    index = index +2
+}
+
+*/
+/*
+let myArray = ["flash","Batman" , "Superman"]
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`values id ${myArray[arr]}`)
+    arr = arr + 1
+}
+*/
+// do while loop
+
+let score = 1
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <=10);
