400354470ee1bca66f8cd00566a4943b5a817e22
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-10-31 06:20:45 +0000

    commit

---
+++ loop/map.js
@@ -0,0 +1,36 @@
+/*
+
+// for of
+
+
+["","",""]
+[{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+for (const num  of arr) {
+    console.log (num)
+}
+
+
+const greetings = "Hello World !!"
+
+for (const greet of greetings) {
+    console.log(`Each charachter is ${greet}`)
+    
+}
+
+*/
+// Map 
+const map = new map();
+map.set('IN',"India")
+map.set('USA',"United stae of america")
+map.set('FR',"France")
+
+
+//console.log(map);
+
+for (const key of map) {
+    console.log(key)
+    
+}
---
+++ loop/three.js
@@ -0,0 +1,12 @@
+const map = new map();
+map.set('IN',"India")
+map.set('USA',"United stae of america")
+map.set('FR',"France")
+
+
+//console.log(map);
+
+for (const key of map) {
+    console.log(key)
+    
+}
