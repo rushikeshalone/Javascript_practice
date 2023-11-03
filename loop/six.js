37b9d37ec0c11e70fee5fd15d299eaf94483570f
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-11-03 07:35:03 +0000

    six

---
+++ loop/six.js
@@ -0,0 +1,16 @@
+/*
+const coading = ["js","ruby","vb.net","python" ,"Rushi"]
+
+
+
+const values = coading.forEach( (item)=>{
+console.log(item);
+})
+
+console.log(values)
+
+*/
+
+const myNum = [1,2,3,4,5,6,7,8,9,10]
+
+myNum.filter(()=>{ })
