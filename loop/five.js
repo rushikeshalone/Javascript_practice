ef6c59f7133b637f4e5c85b9e30bb8e3fa93b785
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-11-03 06:28:32 +0000

    commit

--- loop/five.js
+++ loop/five.js
@@ -1,1 +1,54 @@
-const coading = ["js","ruby","vb.net","python"]
+const coading = ["js","ruby","vb.net","python" ,"Rushi"]
+/*
+for (const key in coading) {
+    console.log(coading [key])
+
+}
+
+*/
+/*
+coading.forEach( function (val){
+console.log(val)
+
+})
+
+*/
+
+/*
+coading.forEach((item)=>{
+    //console.log(item)
+})
+
+
+function printme(item){
+   // console.log(item)
+}
+
+coading.forEach(printme)
+
+*/
+
+coading.forEach((item , index,arr)=>{
+    //console.log(item,index,arr)
+})
+
+
+const myCoading =[
+    {
+        languages : " Javascript",
+        languagesFileName:"js"
+    },
+    {
+        languages : " java",
+        languagesFileName:"javas"
+    },
+    {
+        languages : " python",
+        languagesFileName:"py"
+    }
+]
+
+
+myCoading.forEach((item)=>{
+    console.log(item.languages)
+})
