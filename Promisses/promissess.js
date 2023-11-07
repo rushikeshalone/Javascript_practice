05bf21244520a0cdf7208547246a3e5829f23557
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-11-07 08:35:59 +0000

    Promisses

---
+++ Promisses/promissess.js
@@ -0,0 +1,64 @@
+//fetch('https://something.com').then().catch().finally()
+
+const promiseOne = new Promise(function(resolve , reject){
+// Do an async task
+//DB calls  cryptography  , network
+
+setTimeout(function(){
+    console.log("async task is completed");
+    resolve()
+},1000)
+})
+
+promiseOne.then(function(){
+    console.log("promissess consumed")
+})
+
+// othe method to write promissess
+
+new Promise (function(resolve , reject){
+setTimeout(function(){
+    console.log("async task 2")
+    resolve()
+},1000)
+}).then(function(){
+    console.log("async 2 resolved")
+})
+
+
+// promissess 3 
+
+const promiseThree = new Promise(function(resolve,reject){
+setTimeout(function(){
+resolve({username:"chai",Eamail:"chai@example.com"});
+},1000);
+})
+
+promiseThree.then(function(user){
+console.log(user)
+})
+
+// promise 4 
+
+const promiseFour = new Promise(function(resolve,reject){
+setTimeout(function(){
+    let error = false;
+    if (!error){
+        resolve({username:"rushi",Password:"rushi@gmail.com"})
+    }else{
+        reject('Error: something Went wrong')
+    }
+},2000)
+})
+
+promiseFour.then((user)=>{
+    console.log(user);
+    return user.username
+}).then((username)=>{
+console.log(username)
+}).catch(function(error){
+console.log(error)
+}).finally(()=>{
+    console.log("The Promise is either resolved or reject")
+});
+
