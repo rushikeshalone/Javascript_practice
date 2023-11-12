7786777a2f25b8764b047d9c565d28fab36e39b2
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-11-12 21:59:18 +0000

    call completed

--- call/call.js
+++ call/call.js
@@ -2,17 +2,17 @@
 function setUsername (username){
  // complex DB Call
 this.username = username
+console.log("called")
 }
 
 function createUser(username , email , password){
 
-    setUsername(username)
-
+    setUsername.call(this,username)
     
-
     this.email = email
     this.password = password
 
 }
 
 const chai = new  createUser("chai","chai@gmail.com","123")
+console.log(chai)
