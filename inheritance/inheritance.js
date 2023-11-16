c719198373cb367ea8df39dc76b3414764145163
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-11-16 13:20:40 +0000

    change

---
+++ inheritance/inheritance.js
@@ -0,0 +1,30 @@
+class User {
+constructor(username){
+    this.username = username
+  
+}
+
+ logMe(){
+    console.log(`USERNAME IS ${this.username}`)
+ }
+}
+
+class Teacher extends User{
+    constructor(username,email,Password){
+        super(username)
+        this.email = email
+        this.Password = Password
+    }
+
+    addCourse(){
+        console.log(`A new course was add by ${this.username}`)
+    }
+}
+
+const chai = new Teacher("chai","chai@gmail.com",1245)
+
+chai.addCourse()
+
+const masalaChai = new User ("masalaChai")
+
+masalaChai.logMe()
