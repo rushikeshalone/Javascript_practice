f834e8df53d165d8012c3ef61a64c3f10fcd9b63
Rushikesh Narendra Alone <124777708+rushikeshalone@users.noreply.github.com>
2023-11-16 03:44:07 +0000

    proto

---
+++ class/myclass.js
@@ -0,0 +1,41 @@
+// ES6
+/*
+class User {
+    constructor (username,email,password){
+        this.username = username,
+        this.email = email,
+        this.password = password
+    }
+    encrypPassword(){
+        return `${this.password} abc`
+    }
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const chai = new User("chai","chai @gmail.com","123")
+console.log(chai.encrypPassword());
+console.log(chai.changeUsername());
+
+*/
+
+// Behind  the scene
+
+function User(username,email,password){
+    this.username = username
+    this.email = email,
+    this.password = password
+}
+
+User.prototype.encrypPassword = function(){
+    return  `${this.password} abc`
+}
+
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new User("tea","tea@gmail.com","123")
+console.log(tea.encrypPassword());
+console.log(tea.changeUsername());
